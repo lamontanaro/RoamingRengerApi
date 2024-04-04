93deeaacd18183789c97411cbb41c083aa6ca792
Francisco Lamontanaro <lamontanarofrancisco@gmail.com>
2024-04-04 20:21:30 -0300

    add attractions CRUD

--- controllers/touristAttractionController.js
+++ controllers/touristAttractionController.js
@@ -1,9 +1,54 @@
 const TouristAttraction = require('../models/TouristAttraction');
-
+//POST
+exports.createAttraction = async (req, res) => {
+  try {
+    const newAttraction = await TouristAttraction.create(req.body);
+    res.status(201).json(newAttraction);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+//GET
 exports.getAllAttractions = async (req, res) => {
   try {
     const attractions = await TouristAttraction.find();
     res.json(attractions);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+//GET
+exports.getAttractionbyId = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const attraction = await TouristAttraction.findById(id);
+    res.json(attraction);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+//PUT
+exports.updateAttraction = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedAttraction = await TouristAttraction.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updatedAttraction) {
+      return res.status(404).json({ message: 'Atracción turística no encontrada' });
+    }
+    res.json(updatedAttraction);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+//DELETE
+exports.deleteAttraction = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedAttraction = await TouristAttraction.findByIdAndDelete(id);
+    if (!deletedAttraction) {
+      return res.status(404).json({ message: 'Atracción turística no encontrada' });
+    }
+    res.json({ message: 'Atracción turística eliminada exitosamente' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }

8457caba06e24a36e0889c563f3b357098b26292
Francisco Lamontanaro <lamontanarofrancisco@gmail.com>
2024-04-11 23:55:18 -0300

    add mocks on attraction test

--- tests/turistAttraction.test.js
+++ tests/turistAttraction.test.js
@@ -1,5 +1,6 @@
 const app = require('../app');
 const request = require('supertest');
+const TouristAttraction = require('../models/TouristAttraction')
 
 describe('Create new attraction', () => {
   it('returns status code 201 if valid params are passed', async () => {
@@ -18,6 +19,37 @@ describe('Create new attraction', () => {
   });
 
   it('returns status code 400 if description is missing', async () => {
+    const res = await request(app).post('/touristAttractions').send({ name: 'obelisco' });
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({ "message": "TouristAttraction validation failed: description: Path `description` is required." })
+  });
+});
+
+describe('Create new attraction - using mock', () => {
+  it('returns status code 201 if valid params are passed', async () => {
+    const params = { name: 'zoo', description: 'amazing place to unwind' }
+    TouristAttraction.create = jest.fn().mockResolvedValue(params); 
+
+    const res = await request(app).post('/touristAttractions').send(params);
+
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.body.name).toEqual("zoo");
+  });
+
+  it('returns status code 400 if name is missing', async () => {
+    TouristAttraction.create = jest.fn().mockRejectedValue(new Error('TouristAttraction validation failed: name: Path `name` is required.'));
+
+    const res = await request(app).post('/touristAttractions').send({ description: 'amazing place to unwind' });
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({ "message": "TouristAttraction validation failed: name: Path `name` is required." })
+  });
+
+  it('returns status code 400 if description is missing', async () => {
+    TouristAttraction.create = jest.fn().mockRejectedValue(new Error('TouristAttraction validation failed: description: Path `description` is required.'));
+
     const res = await request(app).post('/touristAttractions').send({ name: 'obelisco' });
 
     expect(res.statusCode).toEqual(400);

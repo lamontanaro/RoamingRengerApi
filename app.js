c89ca18ee8af4cc6f5416f348030554fb89f9056
Francisco Lamontanaro <lamontanarofrancisco@gmail.com>
2024-04-19 19:28:38 -0300

    move ddb url to env vars

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const routes = require('./router');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/touristAttractionsDB', {
+const ddb_url = process.env.DDB_URL;
+
+mongoose.connect(ddb_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
--- models/User.js
+++ models/User.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
     localField: '_id',
     foreignField: 'createdBy'
   });
-  
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;

aa48329b550abf01e867fca0631a06191ab0b7e8
LucasParolini <lucas.parolini47@gmail.com>
2024-05-20 15:59:34 -0300

    add category routes

--- models/TouristAttraction.js
+++ models/TouristAttraction.js
@@ -5,8 +5,11 @@ const touristAttractionSchema = new mongoose.Schema({
   name: { type: String, required : true},
   description: { type: String, required : true},
   location: { type: String, required : true},
-  image: { type: String, required : true}
+  image: { type: String, required : true},
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  belongsTo: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
 });
+
 
 const TouristAttraction = mongoose.model('TouristAttraction', touristAttractionSchema);
 
--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('./services/authMiddleware');
-
 //Controllers imports
 const touristAttractionController = require('./controllers/touristAttractionController');
 const userController = require('./controllers/userController');
+const categoryController = require('./controllers/categoryController');
 
 //Tourist Attractions 
 router.get('/touristAttractions/', authMiddleware.authenticateToken,touristAttractionController.getAllAttractions);
@@ -12,6 +12,10 @@ router.get('/touristAttractions/:id', authMiddleware.authenticateToken,touristAt
 router.post('/touristAttractions/', authMiddleware.authenticateToken,touristAttractionController.createAttractions);
 router.put('/touristAttractions/:id', authMiddleware.authenticateToken,touristAttractionController.updateAttractions);
 router.delete('/touristAttractions/:id', authMiddleware.authenticateToken,touristAttractionController.deleteAttractions);
+
+//Category
+router.post('/category', authMiddleware.authenticateToken,categoryController.createCategory);
+router.get('/attractionsByCategory/:id', authMiddleware.authenticateToken,categoryController.attractionsByCategory);
 
 //User
 router.post('/register/', userController.register);
